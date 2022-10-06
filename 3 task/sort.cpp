1e0026822d0942dae957454b5304916a7a691427
VasSerg <64395125+VasSerg@users.noreply.github.com>
2022-10-07 03:39:07 +0600

    Add files via upload

---
+++ 3 task/sort.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    vector <int> v;
+    string s;
+    
+    cout << "Please write your array separated by spaces: " << endl;
+    getline(cin, s);
+    int i =  0;
+    string num = "";
+    for (char& chr : s) {
+        if (isspace(chr))
+            v.push_back(stoi(num)), num = "", i++;
+        else
+            num += chr;
+    }
+
+    sort(v.begin(), v.end(),[](int a, int b) { return a > b; });
+
+    cout << "Your declining sorted array: " << endl;
+    for (auto i = v.begin(); i != v.end(); ++i)
+        cout << *i << " ";
+    cout << endl;
+
+    sort(v.begin(), v.end(), [](int a, int b) { return a < b; });
+
+    cout << "Your upclining sorted array: " << endl;
+    for (auto i = v.begin(); i != v.end(); ++i)
+        cout << *i << " ";
+
+    cout << endl;
+}
